35b757e1b08755ea9948b36cffb11af822ea0c94
saumayrustagi <31889721+saumayrustagi@users.noreply.github.com>
2025-11-14 21:26:43 +0530

    in-place base32-conversion

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const MAX_SECRET_LENGTH: usize = 20;
+const MAX_SECRET_LENGTH: usize = 32;
 const BASE32_ALPHABET: [u8; 32] = make_base32_alphabet();
 
 const fn make_base32_alphabet() -> [u8; 32] {
@@ -17,20 +17,47 @@ const fn make_base32_alphabet() -> [u8; 32] {
 }
 
 fn make_byte_array(arr: &mut [u8]) {
-    println!("{:?}", arr);
+    println!("Byte Array: {:?}", arr);
+}
+
+fn read_secret_from_file(path: &str) -> String {
+    use std::fs;
+    use std::io::Read;
+
+    let mut buf = vec![0u8; MAX_SECRET_LENGTH];
+
+    let mut file = fs::File::open(path).expect("Failed to open file");
+    let bytes_read = file.read(&mut buf).expect("Error reading from file");
+    buf.truncate(bytes_read);
+
+    let mut secret = String::from_utf8(buf).expect("Expected UTF-8 string");
+    if let Some(first) = secret.lines().next() {
+        secret.truncate(first.len());
+    }
+
+    secret
 }
 
 fn main() {
-    let secret = std::fs::read_to_string(".2fa").unwrap();
-    let secret = secret.trim();
-    println!("{}, {}", str::from_utf8(&BASE32_ALPHABET).unwrap(), secret);
-
-    let mut _arr = [0u8; MAX_SECRET_LENGTH];
-    assert!(secret.len() < MAX_SECRET_LENGTH);
-    let arr: &mut [u8] = &mut _arr[..secret.len()];
-
-    for (i, &byte) in secret.as_bytes().iter().enumerate() {
-        arr[i] = BASE32_ALPHABET.iter().position(|&x| x == byte).unwrap() as u8;
+    let tmp = read_secret_from_file(".2fa");
+    let (offset, len): (usize, usize);
+    {
+        let _tmp = tmp.trim();
+        offset = _tmp.as_ptr() as usize - tmp.as_ptr() as usize;
+        len = _tmp.len();
+        println!(
+            "Received Secret: {}, Offset: {}, Len: {}",
+            _tmp, offset, len
+        );
     }
-    make_byte_array(arr);
+    let secret = &mut tmp.into_bytes()[offset..offset + len];
+
+    for (i, b) in secret.iter_mut().enumerate() {
+        *b = BASE32_ALPHABET
+            .iter()
+            .position(|&x| x == *b)
+            .unwrap_or_else(|| panic!("'{}' at index {} is not in Base32", *b as char, i))
+            as u8;
+    }
+    make_byte_array(secret);
 }
